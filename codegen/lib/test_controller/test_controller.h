7c34bfe5840b426c1025f500b95048bb432b2687
Gustav <g.often94@gmail.com>
2021-04-27 15:19:17 +0200

    tuning

--- codegen/lib/test_controller/test_controller.h
+++ codegen/lib/test_controller/test_controller.h
@@ -21,7 +21,8 @@
 // Function Declarations
 extern void spline_phi_not_empty_init();
 extern double test_controller(double theta, double varphi, double dtheta, double
-  dvarphi, double t);
+  dvarphi, double t, double luenberger_gain);
+extern void test_controller_init();
 
 #endif
 
