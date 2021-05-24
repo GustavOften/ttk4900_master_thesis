07ad14d4ff3a783373bd861cbccc07a6cf7028ec
Gustav <g.often94@gmail.com>
2021-05-24 15:02:46 +0200

    tuning

--- codegen/lib/test_controller/mod.cpp
+++ codegen/lib/test_controller/mod.cpp
@@ -25,11 +25,11 @@ double b_mod(double x)
     r = 0.0;
   } else {
     bool rEQ0;
-    r = std::fmod(x, 6.2831853071795862);
+    r = std::fmod(x, 3.1415926535897931);
     rEQ0 = (r == 0.0);
     if (!rEQ0) {
       double q;
-      q = std::abs(x / 6.2831853071795862);
+      q = std::abs(x / 3.1415926535897931);
       rEQ0 = !(std::abs(q - std::floor(q + 0.5)) > 2.2204460492503131E-16 * q);
     }
 
@@ -37,7 +37,7 @@ double b_mod(double x)
       r = 0.0;
     } else {
       if (x < 0.0) {
-        r += 6.2831853071795862;
+        r += 3.1415926535897931;
       }
     }
   }
