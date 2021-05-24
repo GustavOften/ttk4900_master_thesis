de2e3ce91ef662685d80ac9601c7d96d0b68fba7
Gustav <g.often94@gmail.com>
2021-05-24 14:31:09 +0200

    tuning

--- codegen/lib/test_controller/mod.cpp
+++ codegen/lib/test_controller/mod.cpp
@@ -25,35 +25,6 @@ double b_mod(double x)
     r = 0.0;
   } else {
     bool rEQ0;
-    r = std::fmod(x, 6.2831853071795862);
-    rEQ0 = (r == 0.0);
-    if (!rEQ0) {
-      double q;
-      q = std::abs(x / 6.2831853071795862);
-      rEQ0 = !(std::abs(q - std::floor(q + 0.5)) > 2.2204460492503131E-16 * q);
-    }
-
-    if (rEQ0) {
-      r = 0.0;
-    } else {
-      if (x < 0.0) {
-        r += 6.2831853071795862;
-      }
-    }
-  }
-
-  return r;
-}
-
-double c_mod(double x)
-{
-  double r;
-  if (rtIsNaN(x) || rtIsInf(x)) {
-    r = rtNaN;
-  } else if (x == 0.0) {
-    r = 0.0;
-  } else {
-    bool rEQ0;
     r = std::fmod(x, 3.1415926535897931);
     rEQ0 = (r == 0.0);
     if (!rEQ0) {
--- codegen/lib/test_controller/ppval.cpp
+++ codegen/lib/test_controller/ppval.cpp
@@ -13,8 +13,48 @@
 #include "ppval.h"
 #include "rt_nonfinite.h"
 #include "test_controller.h"
+#include <cstring>
 
 // Function Definitions
+void b_ppval(const double pp_breaks[400], const double pp_coefs[14364], double x,
+             double v[9])
+{
+  if (rtIsNaN(x)) {
+    for (int mid_i = 0; mid_i < 9; mid_i++) {
+      v[mid_i] = x;
+    }
+  } else {
+    int low_i;
+    int mid_i;
+    int low_ip1;
+    int high_i;
+    int icp;
+    double xloc;
+    low_i = 1;
+    low_ip1 = 2;
+    high_i = 400;
+    while (high_i > low_ip1) {
+      mid_i = (low_i + high_i) >> 1;
+      if (x >= pp_breaks[mid_i - 1]) {
+        low_i = mid_i;
+        low_ip1 = mid_i + 1;
+      } else {
+        high_i = mid_i;
+      }
+    }
+
+    icp = (low_i - 1) * 9;
+    xloc = x - pp_breaks[low_i - 1];
+    std::memcpy(&v[0], &pp_coefs[icp], 9U * sizeof(double));
+    for (low_ip1 = 0; low_ip1 < 3; low_ip1++) {
+      high_i = icp + (low_ip1 + 1) * 3591;
+      for (mid_i = 0; mid_i < 9; mid_i++) {
+        v[mid_i] = xloc * v[mid_i] + pp_coefs[high_i + mid_i];
+      }
+    }
+  }
+}
+
 double ppval(const double pp_breaks[500], const double pp_coefs[1996], double x)
 {
   double v;
